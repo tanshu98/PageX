bf06b39a0629d48fe0059bfa9ba7460ea196454d
sharuk abdul <sharuk.abdul@extwebtech.in>
2024-07-02 18:24:14 +0530

    Created the App Component, Added Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/LoginPage/Login';
+import Home from './components/HomePage/Home';
+// import SignUp from './components/SignUp/SignUpFunctional';
+import SignUp from './components/SignUp/SignUp';
+import Header from './components/Header/Header';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
